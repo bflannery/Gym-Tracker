778359e481bc01294dedfbf405d6098ad26e33fb
Brian Flannery <bflannery66@gmail.com>
2016-11-22 14:28:24 -0600

    basic tree

---
+++ app/scripts/main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import router from './router';
+import $ from 'jquery';
+
+let appContainer = document.getElementById('app-container');
